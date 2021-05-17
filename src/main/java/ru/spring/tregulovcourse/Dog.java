dd3e04b728e2ede49e63c2d15aeaa7dbd2c65495
Sergey Rozhkov <borzsonite@gmail.com>
2021-05-17 07:04:03 +0300

    lesson 9

--- src/main/java/ru/spring/tregulovcourse/Dog.java
+++ src/main/java/ru/spring/tregulovcourse/Dog.java
@@ -1,6 +1,10 @@
 package ru.spring.tregulovcourse;
 
 public class Dog implements Pet {
+    public Dog() {
+        System.out.println("Dog bean created");
+    }
+
     @Override
     public void say() {
         System.out.println("Bow-wow");
--- src/main/java/ru/spring/tregulovcourse/Person.java
+++ src/main/java/ru/spring/tregulovcourse/Person.java
@@ -2,9 +2,35 @@ package ru.spring.tregulovcourse;
 
 public class Person {
     private Pet pet;
+    private String name;
+    private int age;
+
+    public Person() {
+
+    }
 
     public Person(Pet pet) {
         this.pet = pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public void call() {
